97f148a37a8f9c203a0c15ebfa5afaf43682a0f1
Blessme Damilola Adegboye <114737807+blessme001@users.noreply.github.com>
2026-02-14 11:11:35 +0100

    Implement dynamic loading of Instagram embeds
    Added functionality to load and display Instagram embeds dynamically.

--- insta-embeds/loader.js
+++ insta-embeds/loader.js
@@ -1,1 +1,20 @@
+const container = document.getElementById('instagram-posts');
 
+const instagramEmbeds = [
+  `<!-- Instagram Post 1 -->
+  <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/POSTCODE1/" data-instgrm-version="14"></blockquote>`,
+  
+  `<!-- Instagram Post 2 -->
+  <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/POSTCODE2/" data-instgrm-version="14"></blockquote>`
+];
+
+instagramEmbeds.forEach(embedHTML => {
+  const div = document.createElement('div');
+  div.innerHTML = embedHTML;
+  container.appendChild(div);
+});
+
+const script = document.createElement('script');
+script.async = true;
+script.src = '//www.instagram.com/embed.js';
+document.body.appendChild(script);
